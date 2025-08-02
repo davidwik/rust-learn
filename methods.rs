8f8faecc35de38fdff6d1c2be0628567fbf567bd
David Viktorsson <david.wiktorsson@gmail.com>
2025-08-02 16:12:39 +0200

    Got started with methods and their relation with struct.

---
+++ methods.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn say_square() -> String {
+        String::from("SQUARE")
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        Rectangle::say_square()
+    );
+}
